9ff46a088ba0c2274c68a350d8cc624bacc96a0e
Mohamed Abdulwahab <mabdulwa@usc.edu>
2021-02-06 22:37:31 -0800

    playing with rotary

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -39,7 +39,6 @@ if __name__ == '__main__':
 
 
     while True:
-      try:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         # time.sleep(0.2)
