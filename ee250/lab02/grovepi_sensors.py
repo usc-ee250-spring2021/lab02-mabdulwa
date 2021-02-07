4dca9dc60fe3f01678b682e907ca55e82e720642
Mohamed Abdulwahab <mabdulwa@usc.edu>
2021-02-06 20:59:42 -0800

    edit LCD

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -38,13 +38,8 @@ if __name__ == '__main__':
 
         print(grovepi.ultrasonicRead(PORT))
 
-        
+
         setRGB(0,255,0)
         buf=list("Grove -Update without erase")
         setText("".join(buf))
         time.sleep(1)
-
-        for i in range(len(buf)):
-          buf[i]="."
-          setText_norefresh("".join(buf))
-          time.sleep(.1)
