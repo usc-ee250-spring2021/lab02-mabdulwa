8b25fc7f5dbdcdb8a96b95d56d3e5a48f72c5e99
Mohamed Abdulwahab <mabdulwa@usc.edu>
2021-02-07 00:39:42 -0800

    changes

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -26,8 +26,6 @@ import grovepi
 from grove_rgb_lcd import *
 
 
-
-
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
@@ -37,39 +35,7 @@ if __name__ == '__main__':
 
     # -- grovepi.pinMode(ultrasonicPort,"INPUT")
     grovepi.pinMode(potentiometer,"INPUT")
-    time.sleep(1)
 
-
-    # ---------
-
-    # Reference voltage of ADC is 5v
-    adc_ref = 5
-
-    # Vcc of the grove interface is normally 5v
-    grove_vcc = 5
-
-    # Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
-    full_angle = 300
-
-    # ---------
-
-    # Read sensor values.
-    potentiometerValue = grovepi.analogRead(potentiometer)
-    ultrasonicValue = str(grovepi.ultrasonicRead(ultrasonicPort))
-
-    # ----
-
-     # Calculate voltage
-     voltage = round((float)(potentiometerValue) * adc_ref / 1023, 2)
-
-    # Calculate rotation in degrees (0 to 300)
-     degrees = str(round((voltage * full_angle) / grove_vcc, 2))
-
-    # ----
-
-
-    setText(" " + degrees + "cm" + " " + "\n" + ultrasonicValue + "cm")
-    setRGB(0,255,128)
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
@@ -78,15 +44,15 @@ if __name__ == '__main__':
 
         print(grovepi.ultrasonicRead(ultrasonicPort))
 
-
+        # Read sensor values.
+        potentiometerValue = grovepi.analogRead(potentiometer)
+        ultrasonicValue = str(grovepi.ultrasonicRead(ultrasonicPort))
         
-        #-- potentiometerDegree = str(round(potentiometerValue / 10))
+        potentiometerDegree = str(round(potentiometerValue / 10))
 
-        if ultrasonicValue < degrees:
-          setText(" " + degrees + "cm" + " " + "OBJ PRES" + "\n" + ultrasonicValue + "cm")
+        if ultrasonicValue < potentiometerDegree:
+          setText(" " + potentiometerDegree + "cm" + " " + "OBJ PRES" + "\n" + ultrasonicValue + "cm")
           setRGB(255,0,0)
-        """
         else:
-          setText(" " + degrees + "cm" + " " + "\n" + ultrasonicValue + "cm")
+          setText(" " + potentiometerDegree + "cm" + " " + "\n" + ultrasonicValue + "cm")
           setRGB(0,255,128)
-          """
